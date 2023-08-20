e99276ec183026b45a006fb4fba1c47e75903426
Phylypo Tum <phylypo@yahoo.com>
2023-08-20 08:29:31 -0700

    add history to template

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ import streamlit as st
 from streamlit_chat import message
 from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+
 from dotenv import load_dotenv
 load_dotenv()
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
+    template2 = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+Current conversation:
+{history}
+Human: {input}
+AI Assistant:"""
+    MY_PROMPT = PromptTemplate(input_variables=["input", "history"], template=template2)
+    llm = ChatOpenAI(temperature=0.0)
+    chain = ConversationChain(llm=llm, prompt=MY_PROMPT, memory=ConversationBufferMemory(ai_prefix="AI Assistant"), verbose=True)
     return chain
 
 chain = load_chain()
